38d457d93f37e9ffbd98424405442d94f92698c8
JasKay <iamjasanyakay@gmail.com>
2019-11-14 20:13:26 -0500

    Commit 11/14/19

---
+++ instangram/src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello Boy</h1>
+// }
+
+const Greet = () => <h1>This is a website </h1>
+
+export default Greet
---
+++ instangram/src/components/Topbar.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Topbar = () => <h4>This is a website </h4>
+
+export default Topbar
